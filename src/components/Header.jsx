ef41310114462ae894fe1d565b142ce39884559e
Gleison Lemos <gleison04lemos@gmail.com>
2025-03-08 04:04:07 -0300

    feat: changing authentication/login event from welcome to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthModal from "../components/AuthModal";
+
 const Header = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+    const [authType, setAuthType] = useState("login");
+    const navigate = useNavigate();
+
+    const openModal = (type) => {
+        setAuthType(type);
+        setModalOpen(true);
+    };
+
+    const handleLoginSuccess = (userData) => {
+        localStorage.setItem("user", JSON.stringify(userData));
+        setModalOpen(false);
+        navigate(`/home`);
+    };
+
     return (
-        <header className="bg-gray-800 text-white text-center p-5">
-        <p>Header</p>
+        <header className="bg-gray-800 p-5 flex justify-between items-center">
+            <h1 className="text-lg font-bold text-gray-600">Agend.io</h1>
+            <div className="flex gap-4">
+                <button
+                    className="bg-blue-500 text-white px-4 py-2 rounded"
+                    onClick={() => openModal("login")}
+                >
+                    Login
+                </button>
+                <button
+                    className="bg-green-500 text-white px-4 py-2 rounded"
+                    onClick={() => openModal("register")}
+                >
+                    Cadastrar-se
+                </button>
+            </div>
+
+            <AuthModal
+                isOpen={modalOpen}
+                onClose={() => setModalOpen(false)}
+                initialType={authType}
+                onLoginSuccess={handleLoginSuccess}
+            />
         </header>
     );
-}
+};
 
 export default Header;
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,50 +1,10 @@
-import AuthModal from "../components/AuthModal";
 import Layout from "../layouts/MainLayout";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
-    const [modalOpen, setModalOpen] = useState(false);
-    const [authType, setAuthType] = useState("login");
-    const navigate = useNavigate();
-
-    const openModal = (type) => {
-        setAuthType(type);
-        setModalOpen(true);
-    };
-
-    const handleLoginSuccess = (userData) => {
-        localStorage.setItem("user", JSON.stringify(userData)); // Salva o usuário no localStorage
-        setModalOpen(false); // Fecha o modal
-        navigate(`/home`); // Redireciona usando o ID do usuário
-    };
-
     return (
         <Layout>
-            <div className="flex justify-end pr-10 gap-4 py-12">
-                <h1>Welcome to Agend.io!</h1>
-                <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-                    <h1 className="text-2xl font-bold mb-4">Bem-vindo ao nosso site!</h1>
-                    <button
-                        className="bg-blue-500 text-white px-4 py-2 rounded mb-2"
-                        onClick={() => openModal("login")}
-                    >
-                        Login
-                    </button>
-                    <button
-                        className="bg-green-500 text-white px-4 py-2 rounded"
-                        onClick={() => openModal("register")}
-                    >
-                        Cadastrar-se
-                    </button>
-
-                    <AuthModal
-                        isOpen={modalOpen}
-                        onClose={() => setModalOpen(false)}
-                        initialType={authType}
-                        onLoginSuccess={handleLoginSuccess} // Passa a função de redirecionamento
-                    />
-                </div>
+            <div className="flex justify-center items-center min-h-screen">
+                <h1 className="text-3xl font-bold">Welcome to Agend.io!</h1>
             </div>
         </Layout>
     );
