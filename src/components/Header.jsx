895d770a02a43ba2f60497bccdd070b1cbc8707a
Gleison Lemos <gleison04lemos@gmail.com>
2025-03-08 13:11:38 -0300

    style: header style preset

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthModal from "../components/AuthModal";
+import ArrowForward from "../components/ui/arrow_forward.svg";
+
 
 const Header = () => {
     const [modalOpen, setModalOpen] = useState(false);
@@ -19,23 +21,33 @@ const Header = () => {
     };
 
     return (
-        <header className="bg-gray-800 p-5 flex justify-between items-center">
-            <h1 className="text-lg font-bold text-gray-600">Agend.io</h1>
-            <div className="flex gap-4">
-                <button
-                    className="bg-blue-500 text-white px-4 py-2 rounded"
-                    onClick={() => openModal("login")}
-                >
-                    Login
-                </button>
-                <button
-                    className="bg-green-500 text-white px-4 py-2 rounded"
-                    onClick={() => openModal("register")}
-                >
-                    Cadastrar-se
-                </button>
-            </div>
+        <header className="">
+            <div className="fixed top-3 left-1/2 transform -translate-x-1/2 bg-white shadow-md py-5 px-12 z-50 w-[40rem] max-w-[95%] rounded-xl">
+                <div className="flex justify-between items-center">
+                    <h1 className="text-xl font-extrabold text-indigo-600">agend.io</h1>
+                    <div className="flex gap-8 pt-1 text-slate-500 font-medium">
+                        <div className="hover:text-slate-700 cursor-pointer">Recursos</div>
+                        <div className="hover:text-slate-700 cursor-pointer">Para empresas</div>
+                        <div className="hover:text-slate-700 cursor-pointer">FAQ</div>
+                    </div>
 
+                    <div className="flex gap-4">
+                        <button
+                            className="bg-indigo-600 text-white flex font-semibold pl-8 itens-center gap-1 pr-5 py-2 rounded-lg hover:bg-indigo-700 hover:cursor-pointer"
+                            onClick={() => openModal("login")}
+                        >
+                            <p className="font-bold">Login</p> <img src={ArrowForward} alt="" className="w-4 pt-1" />
+                        </button>
+                        {/* <button
+                            className="bg-green-500 text-white px-4 py-2 rounded"
+                            onClick={() => openModal("register")}
+                        >
+                            Cadastrar-se
+                        </button> */}
+                    </div>
+
+                </div>
+            </div>
             <AuthModal
                 isOpen={modalOpen}
                 onClose={() => setModalOpen(false)}
@@ -43,6 +55,7 @@ const Header = () => {
                 onLoginSuccess={handleLoginSuccess}
             />
         </header>
+
     );
 };
 
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,7 +3,7 @@ import Layout from "../layouts/MainLayout";
 const Welcome = () => {
     return (
         <Layout>
-            <div className="flex justify-center items-center min-h-screen">
+            <div className="pt-18 flex justify-center items-center min-h-screen">
                 <h1 className="text-3xl font-bold">Welcome to Agend.io!</h1>
             </div>
         </Layout>
