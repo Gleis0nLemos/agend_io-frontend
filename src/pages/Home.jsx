808d15a65fab4c45f105274b0259f2ed98494143
Gleison Lemos <gleison04lemos@gmail.com>
2025-03-11 02:16:12 -0300

    feat: dinamic to view your appointments created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,13 @@ const Home = () => {
     //const { id } = useParams(); // Get the ID from the URL
     const navigate = useNavigate(); // initialize the navigate function
     const [companies, setCompanies] = useState([]);
+    const [appointments, setAppointments] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [user, setUser] = useState(null);
 
     useEffect(() => {
-        const fetchCompanies = async () => {
+        const fetchData = async () => {
             // Retrieve user data and token
             const userData = JSON.parse(localStorage.getItem("user"));
             const token = localStorage.getItem("token");
@@ -46,27 +47,43 @@ const Home = () => {
 
             try {
                 // Do a GET request to the API
-                const response = await fetch(`http://localhost:5000/api/companies`, {
-                    method: "GET",
-                    headers: {
-                        "Authorization": `Bearer ${token}`, // Include the token in the headers
-                        "Content-Type": "application/json"
-                    }
-                });
+                const [companiesResponse, appointmentsResponse] = await Promise.all([
+                    fetch(`http://localhost:5000/api/companies`, {
+                        method: "GET",
+                        headers: {
+                            "Authorization": `Bearer ${token}`, // Include the token in the headers
+                            "Content-Type": "application/json"
+                        },
+                    }),
+                    fetch(`http://localhost:5000/api/appointments`, {
+                        method: "GET",
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                            "Content-Type": "application/json"
+                        },
+                    }),
+                ]);
 
-                if (!response.ok) {
+                if (!companiesResponse.ok) {
                     throw new Error("Erro ao buscar companhias.");
                 }
 
-                const data = await response.json();
-
-                // Check if the data is empty
-                if (!data || data.length === 0) {
-                    setError("Nenhuma companhia encontrada para o ID fornecido.");
-                    return;
+                if (!appointmentsResponse.ok) {
+                    throw new Error("Erro ao buscar agendamentos.");
                 }
 
-                setCompanies(data);
+                const companiesData = await companiesResponse.json();
+                const appointmentsData = await appointmentsResponse.json();
+
+                // // Check if the data is empty
+                // if (!data || data.length === 0) {
+                //     setError("Nenhuma companhia encontrada para o ID fornecido.");
+                //     return;
+                // }
+
+
+                setCompanies(companiesData);
+                setAppointments(appointmentsData);
             } catch (err) {
                 setError(err.message);
             } finally {
@@ -74,7 +91,7 @@ const Home = () => {
             }
         };
 
-        fetchCompanies();
+        fetchData();
     }, [navigate]); // Add navigate to the dependencies array
 
     if (loading) return <p>Carregando...</p>;
@@ -85,43 +102,70 @@ const Home = () => {
 
             <div className="mt-36 w-[1200px] mx-auto">
                 <div className="flex justify-between items-center mb-4">
-                    <div className="flex items-center gap-2">
+                    <div className="flex items-center gap-2 pl-2">
                         <h1 className="text-3xl font-bold">
                             Olá, {user?.name}! 🤙🏼
                         </h1>
                     </div>
                 </div>
 
-                
-                <h2 className="text-lg mb-2 text-gray-600 pl-2 font-semibold">Empresas</h2>
-                <ul className="list-disc grid grid-cols-3">
-                    {companies.map((company) => (
-                        <li
-                            key={company.id}
-                            className="list-none cursor-pointer w-[380px] hover:bg-gray-100 rounded-lg"
-                            onClick={() => navigate(`/companies/${company._id}`)} // Redireciona para detalhes
-                        >
-                            <div className="flex items-center py-4 px-2 gap-4">
-                                <div>
 
-                                <img src={Undefined} alt="Company" className="h-21 w-21 rounded-xl" />
-                                </div>
-                                {/* <img src="https://avatar.iran.liara.run/public" alt="Company" className="h-12 w-12 rounded-full" /> */}
-                                <div>
-                                    <p className="font-medium text-gray-700">{company.name}</p>
-                                    <div className="flex gap-2 text-sm items-center text-slate-400">
-                                        <div className="flex gap-1">
-                                        <img src={Star} alt="" className="h-4 w-4"/>
-                                            4.5
+                <div className="pb-8">
+                    <h2 className="text-lg mb-2 text-gray-600 pl-2 font-semibold">Serviços agendados</h2>
+                    {appointments.length === 0 ? (
+                        <p className="text-gray-500 pl-2">
+                            Nenhum agendamento encontrado.
+                        </p>
+                    ) : (
+                        <ul className="list-none grid grid-cols-4 pl-2 gap-4">
+                            {appointments.map((appointment) => (
+                                <li key={appointment._id} className="hover:bg-gray-100 cursor-pointer p-4 rounded-lg shadow-sm">
+                                    <p className="font-medium text-gray-700">{appointment.serviceId.name}</p>
+                                    <p className="font-medium text-gray-700">R$ {appointment.serviceId.price},00</p>
+                                    <p className="text-sm text-gray-500">
+                                        {new Date(appointment.date).toLocaleString()}
+                                    </p>
+                                    <div className="flex justify-between">
+                                    <p className="text-sm text-gray-500"> {appointment.companyId.name}</p>
+                                    <p className="text-sm text-green-500"> {appointment.status === "pending" ? "Pendente" : "Confirmado"}</p>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+
+                <div className="pb-12">
+                    <h2 className="text-lg mb-2 text-gray-600 pl-2 font-semibold">Empresas</h2>
+                    <ul className="list-disc grid grid-cols-3">
+                        {companies.map((company) => (
+                            <li
+                                key={company.id}
+                                className="list-none cursor-pointer w-[380px] hover:bg-gray-100 rounded-lg"
+                                onClick={() => navigate(`/companies/${company._id}`)} // Redireciona para detalhes
+                            >
+                                <div className="flex items-center py-4 px-2 gap-4">
+                                    <div>
+
+                                        <img src={Undefined} alt="Company" className="h-21 w-21 rounded-xl" />
+                                    </div>
+                                    {/* <img src="https://avatar.iran.liara.run/public" alt="Company" className="h-12 w-12 rounded-full" /> */}
+                                    <div>
+                                        <p className="font-medium text-gray-700">{company.name}</p>
+                                        <div className="flex gap-2 text-sm items-center text-slate-400">
+                                            <div className="flex gap-1">
+                                                <img src={Star} alt="" className="h-4 w-4" />
+                                                4.5
+                                            </div>
+                                            <p>• {company.category} •</p>
+                                            <p>2.3 km</p>
                                         </div>
-                                        <p>• {company.category} •</p>
-                                        <p>2.3 km</p>
                                     </div>
                                 </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </Layout>
     );
