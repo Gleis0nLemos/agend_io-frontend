dadf8624264cd37f555ddbbe9e2cd34ffbd840e4
Gleison Lemos <gleison04lemos@gmail.com>
2025-03-12 21:38:38 -0300

    style: hoursCompany  component preset

--- src/components/HoursCompany.jsx
+++ src/components/HoursCompany.jsx
@@ -1,3 +1,57 @@
-const HoursCompany = () => <div>Horários de funcionamento.</div>;
-
-export default HoursCompany;
+const HoursCompany = () => (
+    <div className="px-8">
+      <div className="grid grid-cols-2 gap-4 text-sm text-gray-500 pr-16">
+        <div className="">
+          <h3>Segunda-feira</h3>
+        </div>
+        <div className="">
+          <p>9h às 18h</p>
+        </div>
+  
+        <div className="">
+          <h3>Terça-feira</h3>
+        </div>
+        <div className="">
+          <p>9h às 18h</p>
+        </div>
+  
+        <div className="">
+          <h3>Quarta-feira</h3>
+        </div>
+        <div className="">
+          <p>9h às 18h</p>
+        </div>
+  
+        <div className="">
+          <h3>Quinta-feira</h3>
+        </div>
+        <div className="">
+          <p>9h às 18h</p>
+        </div>
+  
+        <div className="">
+          <h3>Sexta-feira</h3>
+        </div>
+        <div className="">
+          <p>9h às 18h</p>
+        </div>
+  
+        <div className="">
+          <h3>Sábado</h3>
+        </div>
+        <div className="">
+          <p>9h às 16h</p>
+        </div>
+  
+        <div className="">
+          <h3>Domingo</h3>
+        </div>
+        <div className="">
+          <p>Fechado</p>
+        </div>
+      </div>
+    </div>
+  );
+  
+  export default HoursCompany;
+  
